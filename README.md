214135aff5aa0d6ec6b83160c2628df135c89d9d
Sasha25046 <161481283+Sasha25046@users.noreply.github.com>
2026-03-25 10:25:20 +0000

    .

--- README.md
+++ README.md
@@ -9,8 +9,9 @@ runhaskell task1.hs
 
 sudo apt update && sudo apt install swi-prolog -y
 swipl task1.pl
-move_min_to_front([3, 1, 4, 1, 5], X).
-move_min_to_front([0, -5, 2, -5, 1], X).
-move_min_to_front([2, 5, 8], X).
-move_min_to_front([9, 9, 9], X).
-move_min_to_front([], X).
+start.
+4 1 3 1 2
+0 -5 2 -5 10
+1 2 3 4
+10 20 30 5
+7 7 7 7
--- task1.pl
+++ task1.pl
@@ -1,3 +1,5 @@
+% --- Логіка задачі ---
+
 move_min_to_front([], []).
 move_min_to_front(List, Result) :-
     min_list(List, Min),
@@ -11,3 +13,24 @@ split_min([H|T], Min, [H|Mins], Others) :-
 split_min([H|T], Min, Mins, [H|Others]) :-
     H =\= Min,
     split_min(T, Min, Mins, Others).
+
+% --- Блок для зручного вводу ---
+
+start :-
+    nl, write('--- Задача 1: Мінімум у початок ---'), nl,
+    write('Введіть числа через пробіл (наприклад: 4 1 3 1 2) або stop:'), nl,
+    write('> '),
+    h_flush,
+    read_line_to_string(user_input, String),
+    (   String == "stop" -> write('Вихід.'), nl, !
+    ;   String == "" -> start
+    ;   split_string(String, " ", " ", StrList),
+        (   maplist(number_string, Numbers, StrList) -> 
+                move_min_to_front(Numbers, Res),
+                format('Результат: ~w~n', [Res])
+            ;   write('Помилка: вводьте тільки числа через пробіл!'), nl
+        ),
+        start
+    ).
+
+h_flush :- current_output(Out), flush_output(Out).
